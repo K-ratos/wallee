ba627e488193e3ae9a364b0744497baf7eb8b14d
K-ratos <avinash.kujur121@gmail.com>
2016-10-26 23:10:21 +0530

    version start

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "###############################[K]##################################"
+echo "#			Runs only in i3 			   #"
+echo "####################################################################"
+
+
+echo "are you sure you want to install"
+echo "enter y/n ?"
+read opt
+if [ $opt = "y" ]
+then
+a=`which feh`
+b=`echo $a | grep /user/bin/feh`
+if [ -z $b ]
+then
+echo "because we need to check dependency"
+echo "#-------------Please Enter your password if asked------------#"
+`sudo cp -R ./installable/wallee /usr/bin`
+echo "exec wallee" >> ~/.config/i3/config
+mkdir ~/.config/wallee
+`sudo cp -R ./installable/wallpapers/* ~/.config/wallee`
+echo "wallee has been installed Restart your i3 to see the changes"
+else
+echo "Error : dependency error"
+echo "Please install feh-2.xx " 
+fi
+elif [ $opt = "n" ]
+then
+exit
+fi
+
+
+####################################################################
