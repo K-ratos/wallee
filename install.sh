4f28ad8e84fcfb4fb6177a3c060bec1b6f84ab78
K-ratos <avinash.kujur121@gmail.com>
2017-09-08 20:56:01 +0530

    killall wallee added for bash and zsh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+shell=`echo $SHELL`
 
 echo "###############################[K]##################################"
 echo "#			Runs only in i3 			   #"
 echo "####################################################################"
-
 
 echo "are you sure you want to install"
 echo "enter y/n ?"
@@ -21,6 +21,15 @@ echo "exec wallee" >> ~/.config/i3/config
 mkdir ~/.config/wallee
 `sudo cp -R ./installable/wallpapers/* ~/.config/wallee`
 echo "wallee has been installed Restart your i3 to see the changes"
+if [ $shell = "/bin/bash" ]
+then
+`echo "killall wallee" >> $HOME/.bash_logout
+elif [ $shell = "/bin/zsh" ]
+then
+`echo "killall wallee" >> $HOME/.zlogout`
+else
+echo "Please add 'killall wallee to your logout script of shell'"
+fi
 else
 echo "Error : dependency error"
 echo "Please install feh-2.xx " 
